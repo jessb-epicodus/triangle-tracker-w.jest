700fb11a784ab1e3795da9cc01a824a820268fe5
Jase Seeley <jcseeley31590@yahoo.com>
2022-01-13 15:20:00 -0800

    add if and else if for equilateral and isoceles.

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,19 @@ $(document).ready(function() {
     const side1 = parseInt($("input#side1").val());
     const side2 = parseInt($("input#side2").val());
     const side3 = parseInt($("input#side3").val());
+
+    $("#equilateral").hide();
+    $("#isosceles").hide();
+    $("#scalene").hide();
+    $("#not-triangle").hide();
+    
+    if ((side1 === side2) && (side2 === side3)) {
+    $("#equilateral").show();
+    } else if ((side1 === side2 || side3) && (side2 !== side3)) {
+    $("#isosceles").show();
+    } else if ((side2 === side1 || side3) && (side1 !== side3)) { 
+    $("#isosceles").show();
+    }
+
+  });
+});
