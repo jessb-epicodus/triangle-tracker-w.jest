1ef57a7f551994eec06ee6ca5f2523d53bc71194
jb <jessb.epicodus@gmail.com>
2022-02-01 13:31:10 -0800

    pass test2: return not a triangle

--- __tests__/triangle.test.js
+++ __tests__/triangle.test.js
@@ -8,4 +8,8 @@ describe('Triangle', () => {
     expect(triangle.side2).toEqual(5);
     expect(triangle.side3).toEqual(6);
   });
+  test('should correctly determine whether three lengths are not a triangle', () => {
+    const notTriangle = new Triangle(4,10,23);
+    expect(notTriangle.checkType()).toEqual("not a triangle");
+  });
 });
