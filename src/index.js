f6447b755133f99edbc93f29d3d7608e64fde482
jb <jessb.epicodus@gmail.com>
2022-02-01 10:16:40 -0800

    pass test 1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
+//UI logic
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+
+$(document).ready(function() {
+  $("form#triangle").submit(function(event) {
+    event.preventDefault();
+    const side1 = parseInt($("input#side1").val());
+    const side2 = parseInt($("input#side2").val());
+    const side3 = parseInt($("input#side3").val());
+
+    $("#equilateral").hide();
+    $("#isosceles").hide();
+    $("#scalene").hide();
+    $("#not-triangle").hide();
+    $("#invalid").hide();
+  });
+});
--- src/triangle.js
+++ src/triangle.js
@@ -1,34 +1,27 @@
-$(document).ready(function() {
-  $("form#triangle").submit(function(event) {
-    event.preventDefault();
-    const side1 = parseInt($("input#side1").val());
-    const side2 = parseInt($("input#side2").val());
-    const side3 = parseInt($("input#side3").val());
+//business logic
 
-    $("#equilateral").hide();
-    $("#isosceles").hide();
-    $("#scalene").hide();
-    $("#not-triangle").hide();
-    $("#invalid").hide();
-    
-    if ((side1 &&  side2 && side3 >= 1) && (side1 === side2) && (side2 === side3)) {
-      $("#equilateral").show();
-    } else if ((side1 && side2 && side3 >= 1) && (side1 + side2) <= side3) {
-      $("#not-triangle").show();
-    } else if ((side1 && side2 && side3 >= 1) && (side1 + side3) <= side2) {
-      $("#not-triangle").show();
-    } else if ((side1 && side2 && side3 >= 1) && (side2 + side3) <= side1) {
-      $("#not-triangle").show();
-    } else if ((side1 && side2 && side3 >= 1) && (side1 === side2) && (side1 !== side3)) {
-      $("#isosceles").show();
-    } else if ((side1 && side2 && side3 >= 1) && (side2 === side3) && (side2 !== side1)) { 
-      $("#isosceles").show();
-    } else if ((side1 && side2 && side3 >= 1) && (side3 === side1) && (side3 !== side2)) { 
-      $("#isosceles").show();
-    } else if ((side1 && side2 && side3 >= 1) && (side1 !== side2) && (side2 !== side3)) {
-      $("#scalene").show();
-    } else {
-      $("#invalid").show();
-    }
-  });
-});
+export default function Triangle(side1, side2, side3) {
+  this.side1 = side1;
+  this.side2 = side2;
+  this.side3 = side3;
+}
+
+// if ((side1 &&  side2 && side3 >= 1) && (side1 === side2) && (side2 === side3)) {
+//   $("#equilateral").show();
+// } else if ((side1 && side2 && side3 >= 1) && (side1 + side2) <= side3) {
+//   $("#not-triangle").show();
+// } else if ((side1 && side2 && side3 >= 1) && (side1 + side3) <= side2) {
+//   $("#not-triangle").show();
+// } else if ((side1 && side2 && side3 >= 1) && (side2 + side3) <= side1) {
+//   $("#not-triangle").show();
+// } else if ((side1 && side2 && side3 >= 1) && (side1 === side2) && (side1 !== side3)) {
+//   $("#isosceles").show();
+// } else if ((side1 && side2 && side3 >= 1) && (side2 === side3) && (side2 !== side1)) { 
+//   $("#isosceles").show();
+// } else if ((side1 && side2 && side3 >= 1) && (side3 === side1) && (side3 !== side2)) { 
+//   $("#isosceles").show();
+// } else if ((side1 && side2 && side3 >= 1) && (side1 !== side2) && (side2 !== side3)) {
+//   $("#scalene").show();
+// } else {
+//   $("#invalid").show();
+// }
