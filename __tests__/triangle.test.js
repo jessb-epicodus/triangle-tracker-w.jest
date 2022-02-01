f04527d03a1b9a71076db60e6b0a5a3f95d290b0
jb <jessb.epicodus@gmail.com>
2022-02-01 14:05:50 -0800

    write test 4:return isosceles triangle

--- __tests__/triangle.test.js
+++ __tests__/triangle.test.js
@@ -9,7 +9,15 @@ describe('Triangle', () => {
     expect(triangle.side3).toEqual(6);
   });
   test('should correctly determine whether three lengths are not a triangle', () => {
-    const notTriangle = new Triangle(4,10,23);
+    const notTriangle = new Triangle(3,9,22);
     expect(notTriangle.checkType()).toEqual("not a triangle");
+  });
+  test('should correctly determine whether three lengths make a scalene triangle', () => {
+    const scalTriangle = new Triangle(4,5,7)
+    expect(scalTriangle.checkType()).toEqual("scalene triangle");
+  });
+  test('should correctly determine whether three lengths make an isosceles triangle', () => {
+    const isocTriangle = new Triangle(5,5,7)
+    expect(isocTriangle.checkType()).toEqual("isosceles triangle");
   });
 });
